9949d117cc0d67961242dc9063bbaf2980375db6
itsLyLee <lyylee00@gmail.com>
2020-10-17 20:31:46 +0200

    update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const app = () => {
     const song = document.querySelector('.song');
     const play = document.querySelector('.play');
     const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
 }
 
 app();
