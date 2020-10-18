a41a315a7aa12eaff2b65858f95a21d765e32351
itsLyLee <lyylee00@gmail.com>
2020-10-18 17:47:41 +0200

    update

--- app.js
+++ app.js
@@ -19,19 +19,21 @@ const app = () => {
 
     //Play sound
     play.addEventListener('click', () =>{
-        song.play();
+        checkPlaying(song);
     });
 
-    //Create a function specific to stop and play the sound
+    //A function specific to stop and play the sound
     const checkPlaying = song =>{
         if(song.paused){
         song.play();
+        video.play();
         play.src = './svg/pause.svg';
         }else{
             song.pause();
+            video.pause();
             play.src = './svg.play.svg';
         }
-        }
+    
     }
 };
 
